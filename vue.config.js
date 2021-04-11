891e2f1228b7727ceef440d7890bae4bbdf58144
kleinercubs <1910481653@qq.com>
2021-04-11 16:42:54 +0800

    fix: route issue

--- vue.config.js
+++ vue.config.js
@@ -19,6 +19,9 @@ module.exports = {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
       }
     }
   }
